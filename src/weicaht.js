894e8bc2c3e5427adac2a549ce51d75b0622ed63
ayong <ayongnice@163.com>
2023-04-29 10:41:57 +0800

    修改微信服务接口

--- src/weicaht.js
+++ src/weicaht.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import request from 'request';
+import xml2js from 'xml2js';
+import WechatApi from 'wechat-api';
+import crypto from 'crypto';
+
 const app = express()
-import wechatApi  from 'wechat-api';
+const port = 80;
+
+// 在公众平台中获取的基本配置信息
 
 const config = {
     appId: 'wx3046267608ef4a14',
@@ -9,34 +16,74 @@ const config = {
     encodingAESKey: 'mjePwq5shh0YmFNpI9R0ojMkP5NHsunH1clt0VGHXvX'
 }
 
-const api = new wechatApi(config.appId, config.appSecret)
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// 实例化一个 WechatApi 对象
+const api = new WechatApi(config.appid, config.secret);
 
-app.get('/', (req, res) => {
+// 配置路由，用于接收 GET 请求，进行微信公众号的验证
+app.get('/', function (req, res) {
     console.log('get访问')
-    const { signature, timestamp, nonce, echostr } = req.query
-    const isValid = api.checkSignature({ signature, timestamp, nonce })
-    if (isValid) {
-        res.send(echostr)
-    } else {
-        res.status(401).send('你好')
-    }
-})
+    const query = req.query;
+    const signature = query.signature;
+    const timestamp = query.timestamp;
+    const nonce = query.nonce;
+    const echostr = query.echostr;
+    const token = config.token;
 
-app.post('/', (req, res) => {
+    const array = [token, timestamp, nonce];
+    array.sort();
+    const str = array.join('');
+    const sha1 = crypto.createHash('sha1');
+    sha1.update(str);
+    const result = sha1.digest('hex');
+
+    if (result === signature) {
+        res.send(echostr);
+    } else {
+        res.send('mismatch');
+    }
+});
+
+// 配置路由，用于接收 POST 请求，进行消息的处理和回复
+app.post('/', function (req, res) {
     console.log('post访问')
-    const { signature, timestamp, nonce } = req.query
-    const isValid = api.checkSignature({ signature, timestamp, nonce })
-    if (isValid) {
-        // 在这里进行业务逻辑处理
-        res.send('你好')
-    } else {
-        res.status(401).send('Invalid signature')
-    }
-})
 
-app.listen(80, () => {
-    console.log('Server is running at prot:80')
-})
+    const data = '';
+
+    req.setEncoding('utf8');
+    req.on('data', function (chunk) {
+        data += chunk;
+    });
+    req.on('end', function () {
+        xml2js.parseString(data, {explicitArray: false}, function (err, json) {
+            const fromUsername = json.xml.FromUserName;
+            const toUsername = json.xml.ToUserName;
+            const content = json.xml.Content;
+
+            if (content === '你好') {
+                const replyContent = '您好！欢迎关注我的公众号！';
+                const replyMessage = {
+                    xml: {
+                        ToUserName: fromUsername,
+                        FromUserName: toUsername,
+                        CreateTime: new Date().getTime(),
+                        MsgType: 'text',
+                        Content: replyContent,
+                    },
+                };
+
+                const builder = new xml2js.Builder({rootName: 'xml', cdata: true, headless: true});
+                const xml = builder.buildObject(replyMessage);
+
+                res.send(xml);
+            } else {
+                api.sendText(fromUsername, '我不知道你在说什么！');
+                res.send('');
+            }
+        });
+    });
+});
+
+app.listen(port, function () {
+    console.log('Wechat app listening on port 80!');
+});
