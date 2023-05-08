dd4b22315709b4de6316b03277742927c8144e2a
ayong <ayongnice@163.com>
2023-05-08 20:19:29 +0800

    新增gpt流失结果返回服务

--- src/stream.js
+++ src/stream.js
@@ -1,15 +1,15 @@
-import https from 'https';
 import HttpsProxyAgent from 'https-proxy-agent';
-
-import {WebSocketServer} from 'ws';
 const API_KEY = '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'; // 替换为您的 OpenAI API 密钥
+
+import https from 'https';
+import {WebSocketServer} from 'ws';
 
 
 const proxy = 'http://127.0.0.1:7890'; // 代理地址
 const agent = new HttpsProxyAgent(proxy);
 
 const wsServer = new WebSocketServer({
-    port: 8000
+    port: 80
 });
 
 const options = {
@@ -30,26 +30,34 @@ wsServer.on('connection', function (ws) {
     ws.on('message', async function (message) {
         /** 二进制数据转换 string **/
         const messageData = Buffer.from(message).toString();
-
-        const req = https.request(options, (res) => {
-            res.on('data', (chunk) => {
-                const datachunk = Buffer.from(chunk).toString().replace("data:", "");
-                console.log('datachunk---', datachunk);
-                ws.send(datachunk);
-            });
-        });
-
-        req.on('error', (e) => {
-            console.error(e);
-        });
-        const postData = JSON.stringify({
-            "stream": true,
-            "model": "gpt-3.5-turbo", "messages": [{
-                "role": "user", "content": messageData
-            }], "temperature": 0.7 //此数据 代表这 模型答案匹配精确度  数字越高精度越高
-        });
-        req.write(postData);
-        req.end();
+        ws.send(1);
+        // const req = https.request(options, (res) => {
+        //     res.on('data', (chunk) => {
+        //         const datachunk = Buffer.from(chunk).toString().replace("data:", "");
+        //         const streams = datachunk.trim()
+        //         if(streams !== '[DONE]'){
+        //             // JSON.parse(streams).choices[0].delta.content
+        //             try {
+        //                 console.log('datachunk---',JSON.parse(streams).choices[0].delta.content );
+        //             }catch (e) {
+        //
+        //             }
+        //         }
+        //         ws.send(datachunk);
+        //     });
+        // });
+        //
+        // req.on('error', (e) => {
+        //     console.error(e);
+        // });
+        // const postData = JSON.stringify({
+        //     "stream": true,
+        //     "model": "gpt-3.5-turbo", "messages": [{
+        //         "role": "user", "content": messageData
+        //     }], "temperature": 0.7 //此数据 代表这 模型答案匹配精确度  数字越高精度越高
+        // });
+        // req.write(postData);
+        // req.end();
 
     });
 });
