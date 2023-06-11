08fd1389ce08f0032e18a72cb8e9017bc5307cae
ayong <ayongnice@163.com>
2023-06-11 12:15:18 +0800

    增加 eventSource 接口

--- src/serve/index.js
+++ src/serve/index.js
@@ -8,7 +8,8 @@ import loginRouter from '../login/index.js'
 import registerRouter from '../register/index.js'
 import router from "../login/index.js";
 import rollout from "../rollout/rollout.js";
-
+import url from 'url';
+import querystring from 'querystring';
 // import {router} from '../sma-verify/index.js'
 import http from 'http' ;
 
@@ -40,7 +41,7 @@ app.use('/register', registerRouter);
 app.use('/rollout', rollout);
 
 const proxy = 'http://127.0.0.1:7890'; // 代理地址
-const agent = new HttpsProxyAgent(proxy);
+const agent = new HttpsProxyAgent(proxy);//in environment dev  proxy
 
 const options = {
     hostname: 'api.openai.com',
@@ -53,69 +54,142 @@ const options = {
     agent: agent
 };
 
-app.ws('/websocket', function (ws) {
-    ws.on('message', async function (message) {
+// app.ws('/websocket', function (ws) {
+//     ws.on('message', async function (message) {
+//
+//         /** 二进制数据转换 string **/
+//         const messageData = Buffer.from(message).toString();
+//         /** 前端参数 string **/
+//         const userMessageData = JSON.parse(messageData)
+//         try {
+//             const token = userMessageData.token
+//             // const isTokenExpired = tokenInstance.isTokenExpired(token)
+//             // if (isTokenExpired === 2) {
+//             //     //刷新token
+//             //     const newToken = tokenInstance.refreshUserToken(token)
+//             //     const message = {
+//             //         type: "token",
+//             //         newToken
+//             //     };
+//             //     //返回刷新token
+//             //     // ws.send(JSON.stringify(message));
+//             //     ws.send(JSON.stringify(message));
+//             // }
+//             // ws.clone()
+//             // if (!isTokenExpired) return
+//         } catch (e) {
+//             console.log(e)
+//         }
+//
+//
+//         // websoket 首次验证拦截 gpt转发
+//         if (JSON.parse(messageData).validation) return
+//         const postData = JSON.stringify({
+//             "stream": true,
+//             "model": "gpt-3.5-turbo",
+//             "messages": [{
+//                 "role": "user", "content": userMessageData.data
+//             }], "temperature": 0.7 //此数据 代表这 模型答案匹配精确度  数字越高精度越高
+//         });
+//         // const req = https.request(options, (res) => {
+//         //     res.on('data', (chunk) => {
+//         //         const datachunk = Buffer.from(chunk).toString().replace("data:", "");
+//         //         const streams = datachunk.trim()
+//         //         if (streams !== '[DONE]') {
+//         //             // JSON.parse(streams).choices[0].delta.content
+//         //             try {
+//         //                 console.log('datachunk---', JSON.parse(streams).choices[0].message.content);
+//         //             } catch (e) {
+//         //
+//         //             }
+//         //         }
+//         //         // const newToken = tokenInstance.refreshUserToken(token)
+//         //         console.log('datachunk', datachunk)
+//         //         ws.send(datachunk);
+//         //     });
+//         // });
+//         //
+//         // req.on('error', (e) => {
+//         //     console.error(e);
+//         // });
+//         //
+//         // req.write(postData);
+//         // req.end();
+//
+//
+//     });
+// });
+app.get('/events', (req, res) => {
+    // 设置响应头
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // 跨域访问控制
+    // 发送事件数据
+    const sendEvent = (data) => {
+        res.write(`event: message\n`);
+        res.write(`data: ${data}\n\n`);
+    };
 
-        /** 二进制数据转换 string **/
-        const messageData = Buffer.from(message).toString();
-        /** 前端参数 string **/
-        const userMessageData = JSON.parse(messageData)
-        try {
-            const token = userMessageData.token
-            // const isTokenExpired = tokenInstance.isTokenExpired(token)
-            // if (isTokenExpired === 2) {
-            //     //刷新token
-            //     const newToken = tokenInstance.refreshUserToken(token)
-            //     const message = {
-            //         type: "token",
-            //         newToken
-            //     };
-            //     //返回刷新token
-            //     // ws.send(JSON.stringify(message));
-            //     ws.send(JSON.stringify(message));
-            // }
-            // ws.clone()
-            // if (!isTokenExpired) return
-        } catch (e) {
-            console.log(e)
+    const parsedUrl = url.parse(req.url);
+    const query = querystring.parse(parsedUrl.query);
+    console.log('Received parameters:', query);
+    const queryParameter = JSON.parse(query.messageData)
+    try {
+        const token = queryParameter.token
+        const isTokenExpired = tokenInstance.isTokenExpired(token)
+        console.log('isTokenExpired',isTokenExpired)
+        if (isTokenExpired === 2) {
+            //刷新token
+            const newToken = tokenInstance.refreshUserToken(token)
+            const message = {
+                type: "token",
+                newToken
+            };
+            //返回刷新token
+            sendEvent(JSON.stringify(message))
         }
+        if (!isTokenExpired) return sendEvent(JSON.stringify({message:'！登陆过期，请登录'}))
+    } catch (e) {
+        console.log(e)
+    }
+  
 
 
-        // websoket 首次验证拦截 gpt转发
-        if (JSON.parse(messageData).validation) return
-        const postData = JSON.stringify({
-            "stream": true,
-            "model": "gpt-3.5-turbo",
-            "messages": [{
-                "role": "user", "content": userMessageData.data
-            }], "temperature": 0.7 //此数据 代表这 模型答案匹配精确度  数字越高精度越高
+    const postData = JSON.stringify({
+        "stream": true,
+        "model": "gpt-3.5-turbo",
+        "messages": [{
+            "role": "user", "content": queryParameter.data
+        }], "temperature": 0.7 //此数据 代表这 模型答案匹配精确度  数字越高精度越高
+    });
+    const gptRequest = https.request(options, (res) => {
+        res.on('data', (chunk) => {
+            const datachunk = Buffer.from(chunk).toString().replace("data:", "");
+            const streams = datachunk.trim()
+            if (streams !== '[DONE]') {
+                // JSON.parse(streams).choices[0].delta.content
+                try {
+                    console.log('datachunk---', JSON.parse(streams));
+                    // const newToken = tokenInstance.refreshUserToken(token)
+                    console.log('streams', streams)
+                    sendEvent(datachunk);
+                } catch (e) {
+
+                }
+            }
         });
-        // const req = https.request(options, (res) => {
-        //     res.on('data', (chunk) => {
-        //         const datachunk = Buffer.from(chunk).toString().replace("data:", "");
-        //         const streams = datachunk.trim()
-        //         if (streams !== '[DONE]') {
-        //             // JSON.parse(streams).choices[0].delta.content
-        //             try {
-        //                 console.log('datachunk---', JSON.parse(streams).choices[0].message.content);
-        //             } catch (e) {
-        //
-        //             }
-        //         }
-        //         // const newToken = tokenInstance.refreshUserToken(token)
-        //         console.log('datachunk', datachunk)
-        //         ws.send(datachunk);
-        //     });
-        // });
-        //
-        // req.on('error', (e) => {
-        //     console.error(e);
-        // });
-        //
-        // req.write(postData);
-        // req.end();
+    });
 
+    gptRequest.on('error', (e) => {
+        console.error(e);
+    });
 
+    gptRequest.write(postData);
+    gptRequest.end();
+
+    // 客户端断开连接时清除定时器
+    res.on('close', () => {
     });
 });
 
