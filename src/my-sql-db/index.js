3512b034d16d29388c6ec05e83fff00fd42c4f65
ayong <ayongnice@163.com>
2023-06-22 18:04:57 +0800

    更改数据库名字

--- src/my-sql-db/index.js
+++ src/my-sql-db/index.js
@@ -8,12 +8,12 @@ const fails = () => {
 }
 const succeeds = () => {
 }
-// 创建数据库连接池
+// 创建数据库连接池docker build -t
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'mydatabase',
+    database: 'ayongnicejiayou',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
