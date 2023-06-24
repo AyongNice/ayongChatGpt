c00fb91df75a0840b0fb117465d9bc7e9677f36a
ayong <ayongnice@163.com>
2023-06-24 22:14:25 +0800

    去掉login 500响应 登陆失败响应200

--- src/login/index.js
+++ src/login/index.js
@@ -23,10 +23,11 @@ router.post('/', (req, res) => {
     mysqlDB.login({
         username, password, succeed: (data) => {
             const getToken = token.generateToken(username, data.level || 0, data.amount || 0)
-            res.status(200).json({message: 'Login successful', token: getToken});
+            res.status(200).json({message: 'Login successful', token: getToken, code: 1});
             console.log('login---成功')
         }, fail: (err) => {
             console.log('login---失败', err)
+            res.status(200).json({message: 'Login ---fail', code: 1});
             // res.status(500).json({message: err});
 
         }
